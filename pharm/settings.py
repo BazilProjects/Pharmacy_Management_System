29190b77aa239b16f06d2c9182bb92bc2954f0ce
BazilProjects <154417055+BazilProjects@users.noreply.github.com>
2024-10-11 23:06:35 +0300

    Update settings.py

--- pharm/settings.py
+++ pharm/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['pharmacymanage.herokuapp.com']
 ALLOWED_HOSTS = [
-    'pms-pharmacy-management-system.herokuapp.com',
+    'pms-wjw1.onrender.com',
     '127.0.0.1'
 ]
 
